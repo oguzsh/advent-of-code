12b37cc6534cad24c2b6fe0efba1f7ecd607b3f9
Oguzhan Ince <oguzhan824@gmail.com>
2022-12-03 20:40:39 +0300

    add: december 1

---
+++ 2022/1/part_two.rb
@@ -0,0 +1,5 @@
+path = 'advent_of_code/2022/1/input.txt'
+
+calories = File.readlines(path, chomp: true).slice_before(&:empty?).map { |calorie_group| calorie_group.sum(&:to_i) }
+
+p calories.max(3).sum
